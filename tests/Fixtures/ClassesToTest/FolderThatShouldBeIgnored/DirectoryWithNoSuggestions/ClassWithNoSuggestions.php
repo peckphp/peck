1ab8c17436535191508a1df6808dbb8469381b31
John Evans <john@grandadevans.com>
2024-12-31 11:30:02 +0000

    Prettify the suggestions on the CLI

---
+++ tests/Fixtures/ClassesToTest/FolderThatShouldBeIgnored/DirectoryWithNoSuggestions/ClassWithNoSuggestions.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Fixtures\ClassesToTest\FolderThatShouldBeIgnored\DirectoryWithNoSuggestions;
+
+final class ClassWithNoSuggestions
+{
+    public int $izxuhfdjksfhvWithTypoError = 1;
+}
