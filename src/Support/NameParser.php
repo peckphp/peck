cd9df66902d56a3c72275909668625cf093148e7
Chris Keller <ctk8501@gmail.com>
2025-01-07 09:52:54 +0000

    refactor: remove NameParser::parse() variable assignments

--- src/Support/NameParser.php
+++ src/Support/NameParser.php
@@ -12,18 +12,12 @@ final readonly class NameParser
      */
     public static function parse(string $input): string
     {
-        // Trim leading underscores (e.g. __construct -> construct)
-        $input = ltrim($input, '_');
-
-        // Replace underscores and hyphens with spaces (for snake, screaming snake, and kebab case)
-        $input = str_replace(['_', '-'], ' ', $input);
-
-        // Add spaces before capital letters (for camel and pascal case)
-        $input = (string) preg_replace('/([a-z])([A-Z])/', '$1 $2', $input);
-
-        // Lowercase the input
-        $input = strtolower($input);
-
-        return $input;
+        return strtolower(
+            (string) preg_replace(
+                '/([a-z])([A-Z])/',
+                '$1 $2',
+                str_replace(['_', '-'], ' ', ltrim($input, '_'))
+            )
+        );
     }
 }
