afbd53cc340bff7281dbb18ea67df0ea4bd9a3e6
Nuno Maduro <enunomaduro@gmail.com>
2025-01-13 02:04:09 +0000

    Fixes

--- src/Config.php
+++ src/Config.php
@@ -162,7 +162,7 @@ final class Config
         $filePath = ProjectPath::get().'/'.self::JSON_CONFIGURATION_NAME;
 
         file_put_contents($filePath, json_encode([
-            ...$this->preset ? ['preset' => $this->preset] : [],
+            ...$this->preset !== null ? ['preset' => $this->preset] : [],
             'ignore' => [
                 'words' => $this->whitelistedWords,
                 'paths' => $this->whitelistedPaths,
