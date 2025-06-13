51e75dcc9c15e5a8796a1882e6471c3374f6075c
faissaloux <fwahabali@gmail.com>
2025-06-13 14:36:32 +0100

    make sure all presets stubs have unique values

--- tests/Unit/StubsTest.php
+++ tests/Unit/StubsTest.php
@@ -3,15 +3,13 @@
 declare(strict_types=1);
 
 test('stub files', function (): void {
+    expect('stubs/presets')
+        ->toReturnUnique();
+
     expect('stubs/presets/base.stub')
-        ->toReturnUnique()
         ->toReturnLowercase()
         ->toBeOrdered();
 
-    expect('stubs/presets/iso3166.stub')
-        ->toReturnUnique();
-
     expect('stubs/presets/iso4217.stub')
-        ->toReturnUnique()
         ->toBeOrdered();
 });
