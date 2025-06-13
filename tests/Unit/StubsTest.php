be4f725e0d3fe2fca3321af4c1f2d71174ac5975
faissaloux <fwahabali@gmail.com>
2025-06-13 13:45:54 +0100

    add more tests and fix the stubs

--- tests/Unit/StubsTest.php
+++ tests/Unit/StubsTest.php
@@ -3,7 +3,17 @@
 declare(strict_types=1);
 
 test('stub files', function () {
-    expect('stubs/presets')
+    expect('stubs/presets/base.stub')
         ->toReturnUnique()
+        ->toReturnLowercase()
+        ->toBeOrdered();
+
+    expect('stubs/presets/iso3166.stub')
+        ->toReturnUnique()
+        ->not->toReturnLowercase();
+
+    expect('stubs/presets/iso4217.stub')
+        ->toReturnUnique()
+        ->not->toReturnLowercase()
         ->toBeOrdered();
 });
