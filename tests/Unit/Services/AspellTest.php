be04080c3bc2c465bc587a4353f26306248821ff
Constantin Ross <constantin.ross@stud.hn.de>
2025-09-09 20:53:40 +0200

    feat (aspell): handle misspellings with no suggestions

--- tests/Unit/Services/AspellTest.php
+++ tests/Unit/Services/AspellTest.php
@@ -115,3 +115,16 @@ it('ignores alpha-3 country codes', function (): void {
 
     expect($issues)->toBeEmpty();
 });
+
+it('detects misspellings without suggestions', function (): void {
+    $aspell = new Aspell(
+        Config::instance(),
+        Cache::default(),
+    );
+
+    $misspellings = $aspell->check('Xxxxxxxxxxxxxxxxxx');
+
+    expect($misspellings)->not->toBeEmpty()
+        ->and($misspellings[0]->word)->toBe('Xxxxxxxxxxxxxxxxxx')
+        ->and($misspellings[0]->suggestions)->toBeEmpty();
+});
